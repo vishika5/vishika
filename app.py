f9eacb348c15b85f23ce6eb72d6fba97e090a99d
vishika5 <133037573+vishika5@users.noreply.github.com>
2024-01-08 13:14:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,9 +12,10 @@ with col2:
 with col3:
      st.subheader("suga")
      st.image("./suga.jpg",caption="suga",width=300,use_column_width=True)
-with col4:
-      st.subheader("jhope")
-      st.image("./jhope.jpg",caption="jhope",width=300,use_column_width=True)
+with col3:
+     st.subheader("jhope")
+     st.image("./jhope.jpg",caption="suga",width=300,use_column_width=True)
+
 with col5:
      st.subheader("jimin")
      st.image("./jimin.jpg",caption="jimin",width=300,use_column_width=True)
