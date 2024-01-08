d101417b5abc2cd20b490c9cae1eee72a2c4ff90
vishika5 <133037573+vishika5@users.noreply.github.com>
2024-01-08 14:29:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,22 @@
 import streamlit as st
-st.set_page_config(page_title='bts')
-st.header("BTS")
+st.set_page_config(page_title='BTS')
+st.header("BTS:")
 
 col1,col2,col3,col4,col5,col6,col7=st.columns(7)
 with col1:
-     st.subheader("rm")
-     st.image("./rm.jpg",caption="rm",width=300,use_column_width=True)
+     st.subheader("V ")
+     st.image("./v.jpg",caption="V",width=300,use_column_width=True)
+     
 with col2:
-      st.subheader("jin")
-      st.image("./jin.jpg",caption="jin",width=300,use_column_width=True)
+      st.subheader("JIN ")
+      st.image("./jin.jpg",caption="JIN",width=300,use_column_width=True)
 with col3:
-     st.subheader("suga")
-     st.image("./suga.jpg",caption="suga",width=300,use_column_width=True)
+     st.subheader("RM ")
+     st.image("./rm.jpg",caption="RM",width=300,use_column_width=True)
+     
 with col4:
-     st.subheader("jhope")
-     st.image("./jhope.jpg",caption="jhope",width=300,use_column_width=True)
-
+      st.subheader("JK ")
+      st.image("./jk.jpg",caption="JK",width=300,use_column_width=True)
 with col5:
-     st.subheader("jimin")
-     st.image("./jimin.jpg",caption="jimin",width=300,use_column_width=True)
-with col6:
-      st.subheader("v")
-      st.image("./v.jpg",caption="v",width=300,use_column_width=True)
-with col7:
-      st.subheader("jk")
-      st.image("./jk.jpg",caption="jk",width=300,use_column_width=True)
+     st.subheader("SUGA ")
+     st.image("./suga.jpg",caption="SUGA",width=300,use_column_width=True)
