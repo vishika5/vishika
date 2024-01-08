c200b8f76ef850e6fdf10de7cbdc0e705833b493
vishika5 <133037573+vishika5@users.noreply.github.com>
2024-01-08 14:47:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,28 +4,28 @@ st.header("BTS:")
 
 col1,col2,col3,col4,col5,col6,col7=st.columns(7)
 with col1:
-     st.subheader("V ")
-     st.image("./v.jpg",caption="V",width=300,use_column_width=True)
+     st.subheader("RM ")
+     st.image("./rm.jpg",caption="RM",width=300,use_column_width=True)
      
 with col2:
       st.subheader("JIN ")
       st.image("./jin.jpg",caption="JIN",width=300,use_column_width=True)
 with col3:
-     st.subheader("RM ")
-     st.image("./rm.jpg",caption="RM",width=300,use_column_width=True)
+     st.subheader("SUGA ")
+     st.image("./suga.jpg",caption="SUGA",width=300,use_column_width=True)
      
 
 with col4:
-     st.subheader("SUGA ")
-     st.image("./suga.jpg",caption="SUGA",width=300,use_column_width=True)
+     st.subheader("JHOPE ")
+     st.image("./jhope1.jpg",caption="JHOPE",width=300,use_column_width=True)
 
 with col5:
       st.subheader("JIMIN ")
       st.image("./jimin.jpg",caption="JIMIN",width=300,use_column_width=True)
 with col6:
-     st.subheader("JK ")
-     st.image("./jk1.jpg",caption="SUGA",width=300,use_column_width=True)
+     st.subheader("V ")
+     st.image("./v.jpg",caption="V",width=300,use_column_width=True)
 
 with col7:
-      st.subheader("JHOPE ")
-      st.image("./jhope1.jpg",caption="JIMIN",width=300,use_column_width=True)
+      st.subheader("JK ")
+      st.image("./jk1.jpg",caption="Jk",width=300,use_column_width=True)
